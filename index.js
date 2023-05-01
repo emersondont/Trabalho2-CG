8a884f4cee140d5746685ea1cf861c10f80b38e6
Emerson <emerson.vv75248@gmail.com>
2023-04-30 21:53:34 -0300

    box with rotating light and zoo with 'w' and 'z'

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, fragmentShaderSource);
 gl.compileShader(fragmentShader);
 
-
 const devicePixelRatio = window.devicePixelRatio || 1;
 canvas.width = canvas.clientWidth * devicePixelRatio;
 canvas.height = canvas.clientHeight * devicePixelRatio;
@@ -53,22 +52,24 @@ gl.uniform3f(sphereColor, 1.0, 0.0, 0.0); // vermelho
 gl.uniform3f(cubeColor, 0.0, 0.0, 1.0); // azul
 gl.uniform3f(obstacleColor, 0.0, 1.0, 0.0); // verde
 
-const u_y = gl.getUniformLocation(program, 'u_y');
-gl.uniform1f(u_y, 0.0);
-
 function fract(x) {
     return x - Math.floor(x);
 }
-const xObstacle = gl.getUniformLocation(program, 'u_xObstacle');
+const xObstacle = gl.getUniformLocation(program, 'u_xObstacle')
 
 var jumping = false;
 
 const timeUniformLocation = gl.getUniformLocation(program, 'u_time');
-const random = gl.getUniformLocation(program, 'u_random');
+
+const u_y = gl.getUniformLocation(program, 'u_y');
+gl.uniform1f(u_y, 0.0);
+
+const u_zCam = gl.getUniformLocation(program, 'u_zCam');
+
 var time = 0
-var time_jump = 0
-var x = 8.0;
-var vel = 0.02
+var y = 0.0
+var direction = 'up'
+var zCam = 9.0
 
 document.addEventListener("keydown", function (event) {
     if (event.key === ' ') { // Tecla espaço
@@ -76,30 +77,42 @@ document.addEventListener("keydown", function (event) {
             time_jump = 0
         jumping = true
     }
+    else if(event.key === 'w') {
+        zCam -= 0.5
+    }
+    else if(event.key === 's') {
+        zCam += 0.5
+    }
 });
+
+document.addEventListener("mousemove", function(event) {
+    var posX = event.clientX;
+    var posY = event.clientY;
+    console.log("Posição do mouse - X: " + posX + " Y: " + posY);
+  });
+
+
 
 function render() {
     time += 0.01;
-    time_jump += 0.015;
+    //let y = -10.0 * fract(time_jump * 2) * (fract(time_jump * 2) - 1.0);
 
-    let y = -10.0 * fract(time_jump * 2) * (fract(time_jump * 2) - 1.0);
-    if (jumping) {
-        gl.uniform1f(u_y, y);
+    if(direction == 'up') {
+        y += 0.01
     }
-    if (y <= 0.1) {
-        jumping = false
+    else {
+        y -= 0.01
     }
 
+    if(y >= 2.5) {
+        direction = 'down'
+    }
+    else if( y <= -2.5) {
+        direction = 'up'
+    }
 
-    //vel += vel/1000
-
-    x -= vel
-
-    if (x <= -5.0)
-        x = 8.0
-
-    gl.uniform1f(xObstacle, x);
-
+    gl.uniform1f(u_y, y);
+    gl.uniform1f(u_zCam, zCam);
     gl.uniform1f(timeUniformLocation, time);
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
