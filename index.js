bf2a95dc98fe166ad803dd332043e8b8f4451663
Emerson <emerson.vv75248@gmail.com>
2023-05-08 21:47:44 -0300

    quase pronto, falta os inputs no html

--- index.js
+++ index.js
@@ -40,83 +40,37 @@ gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
 const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
 gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
 
-
 //daqui p baixo
 
-
-const sphereColor = gl.getUniformLocation(program, 'u_sphereColor');
-const cubeColor = gl.getUniformLocation(program, 'u_cubeColor');
-const obstacleColor = gl.getUniformLocation(program, 'u_obstacleColor');
-
-gl.uniform3f(sphereColor, 1.0, 0.0, 0.0); // vermelho
-gl.uniform3f(cubeColor, 0.0, 0.0, 1.0); // azul
-gl.uniform3f(obstacleColor, 0.0, 1.0, 0.0); // verde
-
-function fract(x) {
-    return x - Math.floor(x);
-}
-const xObstacle = gl.getUniformLocation(program, 'u_xObstacle')
-
-var jumping = false;
-
 const timeUniformLocation = gl.getUniformLocation(program, 'u_time');
-
-const u_y = gl.getUniformLocation(program, 'u_y');
-gl.uniform1f(u_y, 0.0);
-
 const u_zCam = gl.getUniformLocation(program, 'u_zCam');
+const u_k = gl.getUniformLocation(program, 'u_k');
 
 var time = 0
-var y = 0.0
-var direction = 'up'
-var zCam = 9.0
+var zCam = 10.0
+var animate = true
+var k = 2.0
 
 document.addEventListener("keydown", function (event) {
-    if (event.key === ' ') { // Tecla espaço
-        if (!jumping)
-            time_jump = 0
-        jumping = true
-    }
-    else if(event.key === 'w') {
+    if(event.key === 'w') {
         zCam -= 0.5
     }
     else if(event.key === 's') {
         zCam += 0.5
     }
+    else if(event.key === ' ') {
+        animate =! animate
+    }
 });
 
-document.addEventListener("mousemove", function(event) {
-    var posX = event.clientX;
-    var posY = event.clientY;
-    console.log("Posição do mouse - X: " + posX + " Y: " + posY);
-  });
-
-
-
 function render() {
-    time += 0.01;
-    //let y = -10.0 * fract(time_jump * 2) * (fract(time_jump * 2) - 1.0);
-
-    if(direction == 'up') {
-        y += 0.01
-    }
-    else {
-        y -= 0.01
-    }
-
-    if(y >= 2.5) {
-        direction = 'down'
-    }
-    else if( y <= -2.5) {
-        direction = 'up'
-    }
-
-    gl.uniform1f(u_y, y);
-    gl.uniform1f(u_zCam, zCam);
+    if(animate)
+        time += 0.2;
+    
     gl.uniform1f(timeUniformLocation, time);
-
+    gl.uniform1f(u_zCam, zCam);
+    gl.uniform1f(u_k, k);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
     requestAnimationFrame(render);
 }
 
