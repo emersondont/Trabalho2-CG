fe03378ba61ec602cd034c80dbccdda5488d7158
Emerson <emerson.vv75248@gmail.com>
2023-05-09 14:39:12 -0300

    tudo pronto

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Trabalho2-CG
+Neste repositório, você encontrará minha implementação do Ray Marching para a disciplina de Computação Gráfica. Ray Marching é uma técnica avançada de renderização em tempo real que permite criar imagens complexas e realistas em ambientes virtuais, utilizando funções matemáticas para renderizar os objetos e formas.
--- index.js
+++ index.js
@@ -43,20 +43,51 @@ gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
 //daqui p baixo
 
 const timeUniformLocation = gl.getUniformLocation(program, 'u_time');
+const u_xCam = gl.getUniformLocation(program, 'u_xCam');
+const u_yCam = gl.getUniformLocation(program, 'u_yCam');
 const u_zCam = gl.getUniformLocation(program, 'u_zCam');
 const u_k = gl.getUniformLocation(program, 'u_k');
 
 var time = 0
+var xCam = 0.0
+var yCam = 0.0
 var zCam = 10.0
 var animate = true
-var k = 2.0
+var k = 5.0
+var vel = 0.1
+
+const inputsmooth = document.getElementById('inputsmooth')
+const inputvel = document.getElementById('inputvel')
+
+inputsmooth.addEventListener("input", function (e) {
+    console.log(e.target.value)
+    k = parseFloat(e.target.value)
+})
+
+inputvel.addEventListener("input", function (e) {
+    console.log(e.target.value)
+    vel = parseFloat(e.target.value)
+})
 
 document.addEventListener("keydown", function (event) {
+    console.log(event.key)
     if(event.key === 'w') {
         zCam -= 0.5
     }
     else if(event.key === 's') {
         zCam += 0.5
+    }
+    else if(event.key === 'a') {
+        xCam -= 0.1
+    }
+    else if(event.key === 'd') {
+        xCam += 0.1
+    }
+    else if(event.key === 'Shift') {
+        yCam += 0.2
+    }
+    else if(event.key === 'Control') {
+        yCam -= 0.2
     }
     else if(event.key === ' ') {
         animate =! animate
@@ -65,9 +96,11 @@ document.addEventListener("keydown", function (event) {
 
 function render() {
     if(animate)
-        time += 0.2;
+        time += vel;
     
     gl.uniform1f(timeUniformLocation, time);
+    gl.uniform1f(u_xCam, xCam);
+    gl.uniform1f(u_yCam, yCam);
     gl.uniform1f(u_zCam, zCam);
     gl.uniform1f(u_k, k);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
